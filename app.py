c58c879660974d515c2b5812d66398d94a18d322
Markz-Zero <tiximon2@gmail.com>
2026-02-26 07:34:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ with col1:
     st.write("choice is an illusion")
     resp1 = st.checkbox('true')
     resp2 = st.checkbox('false')
-    if resp1:
+    if resp1 and not resp2:
        st.write('correct')
-    if resp2:
+    if resp2 and not resp1:
         st.write("you are fooling yourself")
     if resp1 and resp2:
         st.write("you are wise")
@@ -37,3 +37,30 @@ with col2:
        st.write('that is a drink')
     if modo == 'avocado':
        st.write('you are weird')
+
+st.subheader("Buttons")
+if st.button('Press it'):
+    st.write('Why though?')
+else:
+    st.write('Maybe you should')
+
+st.subheader("Selectbox")
+in_mod = st.selectbox(
+    "Selecciona la modalidad",
+    ("Audio", "Visual", "Háptico"),
+)
+if in_mod == "Audio":
+    set_mod = "Reproducir audio"
+elif in_mod == "Visual":
+    set_mod = "Reproducir video"
+elif in_mod == "Háptico":
+    set_mod = "Activar vibración"
+st.write(" La acción es:" , set_mod)
+
+
+with st.sidebar:
+    st.subheader("Configura la modalidad")
+    mod_radio = st.radio(
+        "Escoge la modalidad a usar",
+        ("Visual", "Auditiva","Háptica")
+    )
