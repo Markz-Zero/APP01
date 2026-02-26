dfb651f17cf8d5342e288a5d2ed8e1ca292c022c
Markz-Zero <tiximon2@gmail.com>
2026-02-26 07:09:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("THIS IS A SIMULATION")
+
+st.header("this is a lesser simulation")
+st.write("this one is not a simulation.")
+
+image = Image.open("winds.png")
+st.image(image, caption="background")
